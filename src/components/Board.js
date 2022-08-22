f97057d6a705fe92934310c37d9672caf136b505
aselwilliams <aselwilliams22@gmail.com>
2022-08-22 01:14:59 -0500

    refactored the code

--- src/components/Board.js
+++ src/components/Board.js
@@ -40,38 +40,43 @@ const Board = () => {
     return null;
   };
 
-  const winner =calculateWinner(squares)
-  let status
-  if(winner){
-      status=`Winner is Player ${winner}!`
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = `Winner is Player ${winner}!`;
   } else {
-      status =`Next player: ${isX ? 'X' : 'O'}`
+    status = `Next player: ${isX ? "X" : "O"}`;
   }
 
+  const handleRestart = () => {
+    setIsX(true);
+    setSquares(Array(9).fill(null));
+  };
 
-  const handleRestart=()=>{
-      setIsX(true)
-      setSquares(Array(9).fill(null))
-  }
+  const renderSquare = (i) => {
+    return <Square value={squares[0]} onClick={() => handleClick(i)} />;
+  };
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={squares[0]} onClick={() => handleClick(0)} />
-        <Square value={squares[1]} onClick={() => handleClick(1)} />
-        <Square value={squares[2]} onClick={() => handleClick(2)} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onClick={() => handleClick(3)} />
-        <Square value={squares[4]} onClick={() => handleClick(4)} />
-        <Square value={squares[5]} onClick={() => handleClick(5)} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onClick={() => handleClick(6)} />
-        <Square value={squares[7]} onClick={() => handleClick(7)} />
-        <Square value={squares[8]} onClick={() => handleClick(8)} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
       <div className="status">{status}</div>
-      <button className='restart'onClick={handleRestart} >Restart Game!</button>
+      <button className="restart" onClick={handleRestart}>
+        Restart Game!
+      </button>
     </div>
   );
 };
