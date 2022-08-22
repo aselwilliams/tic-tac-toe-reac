66b26ae3ddf1a2fa2bbef7a942fca8b171917489
aselwilliams <aselwilliams22@gmail.com>
2022-08-21 23:31:05 -0500

    square added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import Square from './components/Square'
+
+//Steps:
+//Create Square Component
+//Create Board Component
+//Display X or O dynamically
+//Calculate Came Winner
+//Show player making the turn
+//Restart the game
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Square value={'X'}/>
     </div>
   );
 }
